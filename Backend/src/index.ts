2f95dafd94c5b05b5023dfa9a1a137bafeb2fce9
manavtore <113475112+manavtore@users.noreply.github.com>
2024-05-03 00:55:56 +0530

    fix:database fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ import updateUser from "./ops/updateUser";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
--- lib/screens/auth/auth/authcontroller.dart
+++ lib/screens/auth/auth/authcontroller.dart
@@ -1,25 +1,37 @@
-//  Future<void> signUp(String email, String password) async {
-//     try {
-//       await auth.createUserWithEmailAndPassword(
-//           email: email, password: password);
+ import 'package:firebase_auth/firebase_auth.dart';
+ import 'package:flutter/material.dart';
+ import 'package:get/get.dart';
+import 'package:healtify/screens/profile/createProfile.dart';
+  FirebaseAuth auth = FirebaseAuth.instance;
+Future<void> signUp(String email, String password) async {
+  
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
 
-//       Get.snackbar("Success", "Account created successfully!");
-//     } catch (e) {
-//       Get.snackbar('Error', e.toString());
-//     }
-//   }
+      const SnackBar(
+        content: Text('Success'),
+      );
+    } catch (e) {
+       SnackBar(
+      content: Text('Error occured $e'),
+    );
+    }
+  }
 
-//   Future<void> signIn(String email, String password) async {
-//     try {
-//       await auth.signInWithEmailAndPassword(email: email, password: password);
-//       Get.snackbar("Success", "Signed in successfully!");
-//     } catch (e) {
-//       Get.snackbar('Error', e.toString());
-//     }
-//   }
+  Future<void> signIn(String email, String password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+      const SnackBar(
+      content: Text('Success! login successful'),
+    );
+    } catch (e) {
+      Get.snackbar('Error', e.toString());
+    }
+  }
 
-//   Future<void> checkUser() async {
-//     if (auth.currentUser != null) {
-//       Get.offAll(() => CreateProfile());
-//     }
-//   }
+  Future<void> checkUser() async {
+    if (auth.currentUser != null) {
+      Get.offAll(() => const CreatProfile());
+    }
+  }
